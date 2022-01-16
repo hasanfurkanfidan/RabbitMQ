4f9c543cd13ee52322ffb2fe3b8c0b0dac94b5aa
hasanfurkanfidan <furkanfidan.job@gmail.com>
2022-01-16 15:00:19 +0300

    Topic Exchange

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -25,31 +25,28 @@ namespace RabbitMQ.Publisher
             var channel = connection.CreateModel();
 
             //channel.QueueDeclare("Hello-Queue", true, false, false);
-            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+            channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
 
-            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
-            {
-                Random random = new Random();
-                LogNames log =(LogNames) random.Next(1, 3);
-                var routeKey = $"route-{x}";
-                var queueName = $"direct-queue-{x}";
-                channel.QueueDeclare(queueName, true, false, false);
-                channel.QueueBind(queueName, "logs-direct", routeKey, null);
-            });
 
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
                 LogNames log = (LogNames)new Random().Next(1, 4);
-                var message = $"log-type : {log}";
+                var random = new Random();
+                var log1 = (LogNames)random.Next(1, 3);
+                var log2 = (LogNames)random.Next(1, 3);
+                var log3 = (LogNames)random.Next(1, 3);
+
+                var routeKey = $"{log1}.{log2}.{log3}";
+                var message = $"log-type : {log1}-{log2}-{log3}";
 
                 //Rabbit MQ ya mesajları byte dizin olarak atıyoruz
 
                 var messageBody = Encoding.UTF8.GetBytes(message);
 
-                var routeKey = $"route-{log}";
+               
 
                 //Default Exchange
-                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+                channel.BasicPublish("logs-topic", routeKey, null, messageBody);
 
                 Console.WriteLine($"Log Gönderilmiştir : {message}");
             });
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace RabbitMQ.Subscriber
 {
@@ -12,41 +13,40 @@ namespace RabbitMQ.Subscriber
 
 
         }
+
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory
             {
                 Uri = new Uri("amqps://wempyzne:cvZP5CHZYcg4aTyEWpnxgIhQL81HBjKK@goose.rmq2.cloudamqp.com/wempyzne")
             };
-            var connection = factory.CreateConnection();
+            using var connection = factory.CreateConnection();
+
             var channel = connection.CreateModel();
 
 
-            //var consumer = new EventingBasicConsumer(_channel);
-
-            //_channel.BasicConsume("Hello-Queue", false, consumer);
-
-            //consumer.Received += (o, eventArgs) =>
-            //{
-            //    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
-            //    Console.WriteLine($"Gelen Mesaj:{message}");
-            //    _channel.BasicAck(eventArgs.DeliveryTag, false);
-            //};
-
-            //var randomQueueName = "logs-database-save";
-            //channel.QueueDeclare(randomQueueName, true, false, false);
-            //channel.QueueBind(randomQueueName, "logs-fanout", string.Empty, null);
-            var queueName = "direct-queue-Critical";
-            var consumer = new EventingBasicConsumer(channel);
             channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            var routekey = "Info.#";
+            channel.QueueBind(queueName, "logs-topic", routekey);
+
             channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Logları dinleniyor...");
+
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
-                Console.WriteLine($"Gelen Mesaj:{message}");
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj:" + message);
+
+                // File.AppendAllText("log-critical.txt", message+ "\n");
+
                 channel.BasicAck(e.DeliveryTag, false);
             };
-            Console.ReadLine();
         }
     }
 }
